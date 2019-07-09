4147d587814d541ca1799f8dbaf81b5a4de618cf
hujiawei <464138803@qq.com>
2019-07-09 18:00:11 +0800

    修改1

--- litemall-admin/src/views/login/index.vue
+++ litemall-admin/src/views/login/index.vue
@@ -8,21 +8,21 @@
         <span class="svg-container svg-container_login">
           <svg-icon icon-class="user" />
         </span>
-        <el-input v-model="loginForm.username" name="username" type="text" auto-complete="on" placeholder="username" />
+        <el-input name="username" type="text" auto-complete="on" placeholder="username" />
       </el-form-item>
 
       <el-form-item prop="password">
         <span class="svg-container">
           <svg-icon icon-class="password" />
         </span>
-        <el-input :type="passwordType" v-model="loginForm.password" name="password" auto-complete="on" placeholder="password" @keyup.enter.native="handleLogin" />
+        <el-input :type="passwordType" name="password" auto-complete="on" placeholder="password" @keyup.enter.native="handleLogin" />
         <span class="show-pwd" @click="showPwd">
           <svg-icon icon-class="eye" />
         </span>
       </el-form-item>
 
       <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>
-
+      <!--
       <div style="position:relative">
         <div class="tips">
           <span> 超级管理员用户名: admin123</span>
@@ -36,7 +36,7 @@
           <span> 推广管理员用户名: promotion123</span>
           <span> 推广管理员用户名：promotion123</span>
         </div>
-      </div>
+      </div> -->
     </el-form>
 
   </div>
